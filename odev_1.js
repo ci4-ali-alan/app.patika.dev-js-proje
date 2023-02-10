14041b8b69148a134c80fae8aaae0b00aa256cab
Ali ALAN <42817177+ci4-ali-alan@users.noreply.github.com>
2023-02-10 09:57:03 +0300

    odev_1

---
+++ odev_1.js
@@ -0,0 +1,10 @@
+let isim = prompt("Lütfen adınızı giriniz.")
+document.getElementById("info").innerHTML = isim;
+
+const clock = new Date();
+document.getElementById("clock").innerHTML = clock.toDateString("tr-TR")
+
+ 
+
+
+
